d55828d3aaff9312cecd04659afb2a8bbc86c5f1
salvatore <salvatore.sorrentino@live.com>
2022-06-21 09:23:57 +0200

    first commit

---
+++ DemoPomeriggioPrism/DemoPomeriggioPrism/App.xaml.cs
@@ -0,0 +1,35 @@
+using DemoPomeriggioPrism.Services;
+using DemoPomeriggioPrism.ViewModels;
+using DemoPomeriggioPrism.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace DemoPomeriggioPrism
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            containerRegistry.Register<IMonkeysService, MonkeyAPIService>();
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<SecondPage, SecondPageViewModel>();
+        }
+    }
+}
---
+++ DemoPomeriggioPrism/DemoPomeriggioPrism/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoPomeriggioPrism.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly INavigationService navigationService;
+
+        public ICommand NavigaCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            NavigaCommand = new Command(Naviga);
+            this.navigationService = navigationService;
+        }
+
+        public async void Naviga()
+        {
+            var navigationParameters = new NavigationParameters();
+            navigationParameters.Add("parametro1", "valore1");
+            await navigationService.NavigateAsync("SecondPage", navigationParameters);
+        }
+    }
+}
